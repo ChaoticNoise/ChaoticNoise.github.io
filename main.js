fb2114bfb2711701306908af039b282220cc335d
breadbeard <dvantuyl@gmail.com>
2016-07-20 12:42:57 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    $(".hamburger").click(function () {
+        if ($("body").hasClass("showSmallMenu")) {
+            $("body").removeClass("showSmallMenu");
+        } else {
+            $("body").addClass("showSmallMenu");
+        }
+    });
+    var removeAll = function () {
+        $("body").removeClass('about');
+        $("body").removeClass('events');
+        $("body").removeClass('booking');
+        $("body").removeClass('fund');
+
+        $("body").removeClass("showSmallMenu");
+        window.scrollTo(0,0);
+    }
+    $(".aboutMenuItem").click(function () {
+        removeAll();
+        $("body").addClass("about");
+    });
+    $(".eventsMenuItem").click(function () {
+        removeAll();
+        $("body").addClass("events");
+    });
+    $(".bookingMenuItem").click(function () {
+        removeAll();
+        $("body").addClass("booking");
+    });
+    $(".smalllogo").click(function () {
+        removeAll();
+    });
+});

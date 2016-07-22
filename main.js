224b29bceb46d298c15bac5397ad2256e0a169ba
breadbeard <dvantuyl@gmail.com>
2016-07-21 21:40:42 -0700

    Add fund raiser page

--- main.js
+++ main.js
@@ -10,24 +10,45 @@
         $("body").removeClass('about');
         $("body").removeClass('events');
         $("body").removeClass('booking');
-        $("body").removeClass('fund');
+        $("body").removeClass('fundRaiser');
 
         $("body").removeClass("showSmallMenu");
         window.scrollTo(0,0);
-    }
+    };
+
+    var setPhotoCredit = function (credit) {
+      credits = {
+        eschewed: { text: 'eschewed', href: 'https://www.instagram.com/eschewed/'},
+        irdeep: { text: 'IRDEEP', href: 'http://irdeep.com/' },
+        espressobuzz: { text: 'EspressoBuzz', href: 'https://www.facebook.com/EspressoBuzzPhotography'}
+      };
+
+      $(".photocredit a").attr("href", credits[credit]['href']);
+      $(".photocredit a").text("Photo credit: " + credits[credit]['text']);
+    };
+
     $(".aboutMenuItem").click(function () {
         removeAll();
         $("body").addClass("about");
+        setPhotoCredit('irdeep');
     });
     $(".eventsMenuItem").click(function () {
         removeAll();
         $("body").addClass("events");
+        setPhotoCredit('espressobuzz');
     });
     $(".bookingMenuItem").click(function () {
         removeAll();
         $("body").addClass("booking");
+        setPhotoCredit('irdeep');
+    });
+    $(".fundRaiserMenuItem").click(function () {
+        removeAll();
+        $("body").addClass("fundRaiser");
+        setPhotoCredit('eschewed')
     });
     $(".smalllogo").click(function () {
         removeAll();
+        setPhotoCredit('irdeep');
     });
 });

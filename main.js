ac0d561c573cc05e8bc03b5c2d307e42e45d51d9
ianpageechols <151470817+ianpageechols@users.noreply.github.com>
2024-11-05 20:58:57 -0800

    added events

--- main.js
+++ main.js
@@ -17,12 +17,47 @@ imageSources.forEach(src => {
 $(document).ready(function () {
     var events = [
 
-        {
+{
             title: "The Bad Things Vaudeville Show",
             url: "https://millerscarnation.com/event/the-bad-things-vaudeville-show/",
             location_display: "Miller's Carnation",
             location_url: "https://maps.app.goo.gl/u5J3nPrZEdc6SAga6",
             time_display: "November 8, 2024, 8:00 PM"
+        },
+        {
+            title: "Georgetown Halloween Parade & Marauding",
+            url: "https://www.georgetowncommunitycouncil.com/post/spooky-parade-to-haunt-georgetown-on-halloween",
+            location_display: "Georgetown, Seattle, WA",
+            location_url: "http://maps.google.com/?q=6525%20Ellis%20Ave%20S.",
+            time_display: "October 31, 2024, 6:30 PM"
+        },
+        {
+            title: "Great Pumpkin Beer Fest 2024",
+            url: "https://www.elysianbrewing.com/gpbf2024",
+            location_display: "Seattle Center",
+            location_url: "http://maps.google.com/?q=Seattle%20Center",
+            time_display: "October 05, 2024, 7:40 PM"
+        },
+        {
+            title: "The LAST EVER Hoodstock",
+            url: "https://www.instagram.com/hotlegsdorsey/p/DA1pi_ezsmU/",
+            location_display: "MLK, Seattle, WA",
+            location_url: "http://maps.google.com/?q=918%20Martin%20Luther%20King%20Jr%20Way",
+            time_display: "October 12, 2024, 3:30 PM"
+        },
+        {
+            title: "Boulderfest competition",
+            url: "https://boulderingproject.com/seattle-boulderfest2024/",
+            location_display: "Seattle Bouldering Project",
+            location_url: "http://maps.google.com/?q=900%20Poplar%20Pl%20S%20Seattle,%20WA%2098144",
+            time_display: "October 12, 2024, 3:30 PM"
+        },
+        {
+            title: "Wally's House of Booze",
+            url: "https://www.instagram.com/explore/locations/461700087651361/wallys-house-of-booze/",
+            location_display: "Miller's Carnation",
+            location_url: "https://maps.app.goo.gl/EGMf7qbZAK9Nem1r9",
+            time_display: "August 30, 2024, 11:00 PM"
         }
         
     ];

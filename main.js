c2120e942130574e18d5fe68e3ca1d0db7e85234
ianpageechols <151470817+ianpageechols@users.noreply.github.com>
2025-01-18 01:52:24 -0800

    fixing an older event url

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
         },
         {
             title: "Port Angeles Wintertide Festival of Lights",
-            url: "pafac.kindful.com/e/wintertide-2024",
+            url: "http://pafac.kindful.com/e/wintertide-2024",
             location_display: "Port Angeles Fine Arts Center",
             location_url: "https://maps.app.goo.gl/2rAiAaNYwRL7ZNhB8",
             time_display: "December 7, 2024, 4:00 PM"

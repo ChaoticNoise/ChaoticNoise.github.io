b2a101e80b8c78e13cede7ae226f38da97556cd1
ianpageechols <151470817+ianpageechols@users.noreply.github.com>
2025-06-12 02:47:33 -0700

    updating the code from the working local copy

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
 title: "Wally's House of Booze",
 url: "https://www.instagram.com/explore/locations/461700087651361/wallys-house-of-booze/",
-location_display: "Miller's Carnation",
+location_display: "Wally's Tavern",
 location_url: "https://maps.app.goo.gl/EGMf7qbZAK9Nem1r9",
 time_display: "August 30, 2024, 11:00 PM"
 
@@ -30,7 +30,8 @@ title: "Great Pumpkin Beer Fest 2024",
 url: "https://www.elysianbrewing.com/gpbf2024",
 location_display: "Seattle Center",
 location_url: "https://maps.google.com/?q=Seattle%20Center",
-time_display: "October 05, 2024, 7:40 PM"
+time_display: "October 05, 2024, 7:40 PM",
+image: "http://chaoticnoise.com/event_fliers/elysian-pumpkin-fest-2024.jpg"
 
 },{
 
@@ -38,7 +39,8 @@ title: "The LAST EVER Hoodstock",
 url: "https://www.instagram.com/hotlegsdorsey/p/DA1pi_ezsmU/",
 location_display: "MLK, Seattle, WA",
 location_url: "https://maps.google.com/?q=918%20Martin%20Luther%20King%20Jr%20Way",
-time_display: "October 12, 2024, 3:30 PM"
+time_display: "October 12, 2024, 3:30 PM",
+image: "http://chaoticnoise.com/event_fliers/20241012_Hoodstock.jpg"
 
 },{
 
@@ -46,7 +48,8 @@ title: "Boulderfest competition",
 url: "https://boulderingproject.com/seattle-boulderfest2024/",
 location_display: "Seattle Bouldering Project",
 location_url: "https://maps.google.com/?q=900%20Poplar%20Pl%20S%20Seattle,%20WA%2098144",
-time_display: "October 12, 2024, 6:00 PM"
+time_display: "October 12, 2024, 6:00 PM",
+image: "http://chaoticnoise.com/event_fliers/20241012_Boulderfest.jpg"
 
 },{
 
@@ -54,7 +57,8 @@ title: "Georgetown Halloween Parade & Marauding",
 url: "https://www.georgetowncommunitycouncil.com/post/spooky-parade-to-haunt-georgetown-on-halloween",
 location_display: "Georgetown, Seattle, WA",
 location_url: "https://maps.google.com/?q=6525%20Ellis%20Ave%20S",
-time_display: "October 31, 2024, 6:30 PM"
+time_display: "October 31, 2024, 6:30 PM",
+image: "http://chaoticnoise.com/event_fliers/20241013_Georgetown_Halloween_Parade.jpg"
 
 },{
 
@@ -62,7 +66,8 @@ title: "The Bad Things Vaudeville Show",
 url: "https://millerscarnation.com/event/the-bad-things-vaudeville-show/",
 location_display: "Miller's Carnation",
 location_url: "https://maps.app.goo.gl/u5J3nPrZEdc6SAga6",
-time_display: "November 8, 2024, 8:00 PM"
+time_display: "November 8, 2024, 8:00 PM",
+image: "http://chaoticnoise.com/event_fliers/20241108_Bad_Things_Vaudeville_Show.jpg"
 
 },{
 
@@ -70,7 +75,8 @@ title: "Pun Slam",
 url: "https://www.eventbrite.com/e/all-stars-one-wild-card-fun-intended-punslam-comedy-wordplay-show-tickets-1045396056747?aff=erelexpmlt",
 location_display: "Capitol Hill Comedy / Bar",
 location_url: "https://maps.app.goo.gl/69YvGQpNynpnJqnTA",
-time_display: "November 27, 2024, 7:00 PM"
+time_display: "November 27, 2024, 7:00 PM",
+image: "http://chaoticnoise.com/event_fliers/20241127_Pun_Slam.jpg"
 
 },{
 
@@ -86,7 +92,8 @@ title: "Equinox Very Open House",
 url: "https://www.facebook.com/profile.php?id=100063803760373",
 location_display: "Equinox Studios",
 location_url: "https://maps.app.goo.gl/ZA1E8ZcNFWpvpRc36",
-time_display: "December 14, 2024, 3:00 PM"
+time_display: "December 14, 2024, 3:00 PM",
+image: "http://chaoticnoise.com/event_fliers/20241214_Equinox_Open_House.jpg"
 
 },{
 
@@ -102,7 +109,8 @@ title: "Big Brass Extravaganza #11 feat: This Much Brass, Chaotic Noise Marching
 url: "https://www.tixr.com/groups/nectarlounge/events/big-brass-extravaganza-11-feat-this-much-brass-chaotic-noise-marching-corps-vamol-agab-analog-brass-124702",
 location_display: "Nectar Lounge",
 location_url: "https://maps.app.goo.gl/HVsYnea9D6iVrdDz9",
-time_display: "January 16, 2025, 7:00 PM"
+time_display: "January 16, 2025, 7:00 PM",
+image: "http://chaoticnoise.com/event_fliers/20250116_Big_Brass_Extravaganza_promo.jpg"
 
 },{
 
@@ -166,7 +174,8 @@ title: "Georgetown Carnival",
 url: "https://georgetownseattle.org/georgetown-carnival-2025/",
 location_display: "Georgetown, Seattle, WA",
 location_url: "https://maps.app.goo.gl/RVrS9C7Aeb4AtgxY9",
-time_display: "June 14, 2025, 12:00 PM"
+time_display: "June 14, 2025, 12:00 PM",
+image: "http://chaoticnoise.com/event_fliers/GT-Carnival-2025-Postcard-Front-683x1024.jpg"
 
 },{
 
@@ -174,7 +183,8 @@ title: "Clock-Out Lounge with the Bad Things",
 url: "https://clockoutlounge.com",
 location_display: "Clock-Out Lounge",
 location_url: "https://maps.app.goo.gl/5bgBVNwpA4sqRcrx8",
-time_display: "June 14, 2025, 7:00 PM"
+time_display: "June 14, 2025, 7:00 PM",
+image: "http://chaoticnoise.com/event_fliers/20250614_The_Bad_Things_Vaudeville_Show.jpg"
 
 },{
 
@@ -182,7 +192,8 @@ title: "Death by Brass at The High Water Mark with Brassless Chaps, Couch Brass,
 url: "https://maps.app.goo.gl/AP9kQP8VkCbQmKfE9",
 location_display: "Clock-Out Lounge",
 location_url: "https://maps.app.goo.gl/AP9kQP8VkCbQmKfE9",
-time_display: "June 19, 2025, 8:30 PM"
+time_display: "June 19, 2025, 8:30 PM",
+image: "http://chaoticnoise.com/event_fliers/20250619_Death_by_Brass_with_Brassless_Chaps,_Couch_Brass,_and_Chaotic_Noise_at_The_High_Water_Mark_in_Portland.jpg"
 
 },{
 
@@ -213,7 +224,11 @@ time_display: "February 13, 2026, 12:00 PM"
         // Check if event is in the past
         if (eventDate < today) {
 // Past event
+
+var imageUrl = event.image || "http://chaoticnoise.com/event_fliers/placeholder_event_image.png";
+
 var eventHtml = "<div class='event past-event'>" +
+    "<img src='" + imageUrl + "' class='event-image' alt='Event flier'>" +
     "<div class='eventName'>" +
     "<a href='" + event.url + "' target='_blank'>" + event.title + "</a>" +
     "</div>" +
@@ -227,7 +242,11 @@ var eventHtml = "<div class='event past-event'>" +
 $("#past-events .eventsInfo").append(eventHtml);
         } else {
 // Upcoming event
+
+var imageUrl = event.image || "http://chaoticnoise.com/event_fliers/placeholder_event_image.png";
+
 var eventHtml = "<div class='event'>" +
+    "<img src='" + imageUrl + "' class='event-image' alt='Event flier'>" +
     "<div class='eventName'>" +
     "<a href='" + event.url + "' target='_blank'>" + event.title + "</a>" +
     "</div>" +
@@ -248,13 +267,12 @@ $("#upcoming-events .eventsInfo").append(eventHtml);
 // Set the current year in the copyright section
 
 // Wait until the document is fully loaded
-document.addEventListener("DOMContentLoaded", function() {
-    // Set the current year in the copyright section
-    document.getElementById("current-year").textContent = new Date().getFullYear();
-});
 
-// Toggle Past Events Section
 document.addEventListener("DOMContentLoaded", function () {
+    // Set the current year
+    document.getElementById("current-year").textContent = new Date().getFullYear();
+
+    // Toggle Past Events section
     const toggle = document.getElementById("toggle-past-events");
     const container = document.getElementById("past-events-container");
     const caret = toggle.querySelector(".caret");
@@ -263,5 +281,27 @@ document.addEventListener("DOMContentLoaded", function () {
         const isVisible = container.style.display === "block";
         container.style.display = isVisible ? "none" : "block";
         caret.classList.toggle("expanded", !isVisible);
+    });
+
+    // Lightbox functionality with event delegation
+    const lightbox = document.getElementById("lightbox-overlay");
+    const lightboxImg = lightbox.querySelector("img");
+
+    document.body.addEventListener("click", function (e) {
+        const target = e.target;
+
+        // Check if the clicked element is an event image
+        if (target.classList.contains("event-image")) {
+            const src = target.src;
+            if (src.includes("placeholder_event_image")) return;
+
+            lightboxImg.src = src;
+            lightbox.style.display = "flex";
+        }
+    });
+
+    lightbox.addEventListener("click", function () {
+        lightbox.style.display = "none";
+        lightboxImg.src = "";
     });
 });
